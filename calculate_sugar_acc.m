6f10e83ea3b7c224e98dee37f6b5e37b585a4ad5
danielseaton <daniel.seaton87@gmail.com>
2018-05-27 22:43:46 +0100

    annotation

--- calculate_sugar_acc.m
+++ calculate_sugar_acc.m
@@ -1,4 +1,18 @@
 function [efficiency,growth_capacity]=calculate_sugar_acc(t,p,eme,efficiency,growth_capacity,is_light)
+%% calculate sugar accumulation
+%
+% Input:
+%   t - current time
+%   p - vector of model parameters
+%   eme - time of emergence
+%   efficiency - timeseries vector of photosynthetic efficiency
+%   growth_capacity - timeseries vector of growth capacity
+%   is_light - timeseries vector specifying when there is light
+%
+% Output:
+%   efficiency - timeseries vector of photosynthetic efficiency
+%   growth_capacity - timeseries vector of growth capacity
+
 if t < eme + 24 + 1
     efficiency(t) = p(84);
     growth_capacity(t) = p(63);
